16df96bc339e79c4c01b35b47b6ec271210dc0b2
Douglas Martins <douglas.martins@auditornfe.com.br>
2023-03-10 10:40:52 -0300

    Feat: Utilizando o debug do vs code

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,15 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.json({ message: "Hello World!" });
+})
+
+app.post("/courses", (req, res) => {
+    const { name } = req.body;
+    res.json({ name });
 })
 
 app.listen(3333, () => {
